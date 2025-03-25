6f0d302322fadc12483872fc237f0eb744d38a45
Nikita <nik.shmagin@yandex.ru>
2025-03-25 18:33:08 +0300

    Добавил страницу входа/регистрации и сделал гиперссылки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const location = useLocation();
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
-        <div className={styles.logo}>
-          <img src="/images/sputnik.png" alt="Спутник" />
-        </div>
-        <div className={styles.siteName}>Watercraft Detector</div>
+        <Link to="/" className={styles.logolink}>
+            <div className={styles.logo}>
+                <img src="/images/sputnik.png" alt="Спутник" />
+            </div>
+            <div className={styles.siteName}>Watercraft Detector</div>
+        </Link>
         <nav className={styles.nav}>
           <button className={styles.navButton}>
             <a href="#about" className={styles.navLink}>О нас</a>
@@ -16,9 +20,11 @@ const Header = () => {
           <button className={styles.navButton}>
             <a href="#tech" className={styles.navLink}>Технологии</a>
           </button>
-          <button className={styles.navButton}>
-            <a href="#contact" className={styles.navLink}>Вход/Регистрация</a>
-          </button>
+          {location.pathname !== '/auth' && (
+            <button className={styles.navButton}>
+                <a href="/auth" className={styles.navLink}>Вход/Регистрация</a>
+            </button>
+          )}
         </nav>
       </div>
     </header>
